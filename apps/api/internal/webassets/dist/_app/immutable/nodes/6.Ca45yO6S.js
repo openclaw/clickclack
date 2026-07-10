fc2cfc79c356484f563f41d4fdabee02a26c9b12
Shakker <shakkerdroid@gmail.com>
2026-07-10 13:53:33 +0100

    build: refresh embedded settings assets

--- apps/api/internal/webassets/dist/_app/immutable/nodes/6.Ca45yO6S.js
+++
@@ -1,1 +0,0 @@
-import{ht as e,mt as t}from"../chunks/ns2AJC_8.js";import"../chunks/xihTtKlq.js";import{t as n}from"../chunks/B534o9pb.js";function r(r,i){e(i,!0),n(r,{get routeWorkspaceID(){return i.params.workspaceID},get routeTargetID(){return i.params.targetID}}),t()}export{r as component};
